a9c67a34ab82c536e8056dab67457c09571f1939
Pavel <magma4810@gmail.com>
2023-11-14 18:54:25 +0300

    add tests for exercise number seven

--- src/homeworksOtus.js
+++ src/homeworksOtus.js
@@ -43,3 +43,37 @@ export function objectUser(ageNum){
     const admin = Object.assign({role: 'admin'},user);
     return admin
 }
+export function makeInteractiveList(div){
+    const body = document.body;
+    body.append(div)
+    const input = document.createElement('input');
+    div.append(input);
+    const button = document.createElement('button');
+    button.innerHTML = 'Add';
+    button.hidden = true;
+    div.append(button);
+    const list = [];
+    const listBody = document.createElement('ol');
+    div.append(listBody);
+    for (let i = 0;i<3;i++){
+        list.push(`${i+1}`.repeat(i+1))
+    }
+    button.addEventListener('click',()=>{
+        list.push(input.value);
+        input.value = '';
+        button.hidden = true;
+        if(list.length > 5){
+            list.shift();
+        }
+        listBody.innerHTML = list.map((el)=> (`<li>${el}</li>`)).join('');
+    })
+    listBody.innerHTML = list.map((el)=> (`<li>${el}</li>`)).join('');
+    input.addEventListener('input', ()=>{
+        if(input.value !== ''){
+            button.hidden = false;
+        }else{
+            button.hidden = true;
+        }
+    })
+    
+}
--- src/simplefile.js
+++ src/simplefile.js
@@ -1,9 +1,35 @@
-function objectUser(){
-    const ageNum = prompt('enter age');
-    const user = {
-        age: ageNum,
+function makeInteractiveList(div){
+    const body = document.body;
+    body.append(div)
+    const input = document.createElement('input');
+    div.append(input);
+    const button = document.createElement('button');
+    button.innerHTML = 'Add';
+    button.hidden = true;
+    div.append(button);
+    const list = [];
+    const listBody = document.createElement('ol');
+    div.append(listBody);
+    for (let i = 0;i<3;i++){
+        list.push(`${i+1}`.repeat(i+1))
     }
-    const admin = Object.assign({role: 'admin'},user);
-    return [user,admin]
+    button.addEventListener('click',()=>{
+        list.push(input.value);
+        input.value = '';
+        button.hidden = true;
+        if(list.length > 5){
+            list.shift();
+        }
+        listBody.innerHTML = list.map((el)=> (`<li>${el}</li>`)).join('');
+    })
+    listBody.innerHTML = list.map((el)=> (`<li>${el}</li>`)).join('');
+    input.addEventListener('input', ()=>{
+        if(input.value !== ''){
+            button.hidden = false;
+        }else{
+            button.hidden = true;
+        }
+    })
+    
 }
-console.log(objectUser())
+makeInteractiveList(document.createElement('div'))
