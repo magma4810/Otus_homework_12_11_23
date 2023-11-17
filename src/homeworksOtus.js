c206a8a79f21a02bcd938f653cfd0d39f13515c5
Pavel <magma4810@gmail.com>
2023-11-17 20:47:44 +0300

    add connected lint and husky

--- src/homeworksOtus.js
+++ src/homeworksOtus.js
@@ -1,120 +1,127 @@
-export function sumNum(num){
-    return num.split('').map((el)=>Number(el)).reduce((el,sum)=>sum+=el)
+export function sumNum(num) {
+  return num
+    .split("")
+    .map((el) => Number(el))
+    .reduce((el, sum) => (sum += el));
 }
-export function maxNum(a,b){
-    return Math.max(a,b);
+export function maxNum(a, b) {
+  return Math.max(a, b);
 }
-export function returnMounth(num){
-    const date = new Date(`2022-${num}-15`);
-    const monthName = date.toLocaleString('default', { month: 'long' });
-    return monthName;
+export function returnMounth(num) {
+  const date = new Date(`2022-${num}-15`);
+  const monthName = date.toLocaleString("default", { month: "long" });
+  return monthName;
 }
-export function sumFrom50_Before100(){
-    let summa = 50;
-    for (let i = 50;i<101;i++){
-        summa += i
+export function sumFrom50_Before100() {
+  let summa = 50;
+  for (let i = 50; i < 101; i++) {
+    summa += i;
+  }
+  return summa;
+}
+export function multiplicationTableForSeven() {
+  const table = [];
+  for (let i = 1; i < 11; i++) {
+    table.push(`7 * ${i} = ${7 * i}`);
+  }
+  return table;
+}
+export function averageAllOddNum(n) {
+  let quanOddNum = 0;
+  let sumQuanOddNum = 0;
+  for (let i = 1; i < n + 1; i++) {
+    if (i % 2 === 0) {
+      quanOddNum++;
+      sumQuanOddNum += i;
     }
-    return summa;
+  }
+  return sumQuanOddNum / quanOddNum;
 }
-export function multiplicationTableForSeven(){
-    const table = [];
-    for(let i = 1;i<11;i++){
-        table.push(`7 * ${i} = ${7*i}`)
-    }
-    return table
+export function objectUser(ageNum) {
+  // const ageNum = prompt('enter age');
+  const user = {
+    age: ageNum,
+  };
+  const admin = Object.assign({ role: "admin" }, user);
+  return admin;
 }
-export function averageAllOddNum(n){
-    let quanOddNum = 0;
-    let sumQuanOddNum = 0;
-    for (let i = 1;i<n+1;i++){
-        if (i%2 === 0){
-            quanOddNum++;
-            sumQuanOddNum += i;
-        }
-    }
-    return sumQuanOddNum/quanOddNum;
-}
-export function objectUser(ageNum){
-    // const ageNum = prompt('enter age');
-    const user = {
-        age: ageNum,
-    }
-    const admin = Object.assign({role: 'admin'},user);
-    return admin
-}
-export function makeInteractiveList(div){
-    const body = document.body;
-    body.append(div)
-    const input = document.createElement('input');
-    div.append(input);
-    const button = document.createElement('button');
-    button.innerHTML = 'Add';
+export function makeInteractiveList(div) {
+  const body = document.body;
+  body.append(div);
+  const input = document.createElement("input");
+  div.append(input);
+  const button = document.createElement("button");
+  button.innerHTML = "Add";
+  button.hidden = true;
+  div.append(button);
+  const list = [];
+  const listBody = document.createElement("ol");
+  div.append(listBody);
+  for (let i = 0; i < 3; i++) {
+    list.push(`${i + 1}`.repeat(i + 1));
+  }
+  button.addEventListener("click", () => {
+    list.push(input.value);
+    input.value = "";
     button.hidden = true;
-    div.append(button);
-    const list = [];
-    const listBody = document.createElement('ol');
-    div.append(listBody);
-    for (let i = 0;i<3;i++){
-        list.push(`${i+1}`.repeat(i+1))
+    if (list.length > 5) {
+      list.shift();
     }
-    button.addEventListener('click',()=>{
-        list.push(input.value);
-        input.value = '';
-        button.hidden = true;
-        if(list.length > 5){
-            list.shift();
-        }
-        listBody.innerHTML = list.map((el)=> (`<li>${el}</li>`)).join('');
-    })
-    listBody.innerHTML = list.map((el)=> (`<li>${el}</li>`)).join('');
-    input.addEventListener('input', ()=>{
-        if(input.value !== ''){
-            button.hidden = false;
-        }else{
-            button.hidden = true;
-        }
-    })
+    listBody.innerHTML = list.map((el) => `<li>${el}</li>`).join("");
+  });
+  listBody.innerHTML = list.map((el) => `<li>${el}</li>`).join("");
+  input.addEventListener("input", () => {
+    if (input.value !== "") {
+      button.hidden = false;
+    } else {
+      button.hidden = true;
+    }
+  });
 }
-export function sumArray(n){
-    return n.reduce((el,sum)=>sum+=el)
+export function sumArray(n) {
+  return n.reduce((el, sum) => (sum += el));
 }
-export function doubleArray(n){
-    return n.map((el)=>el*2)
+export function doubleArray(n) {
+  return n.map((el) => el * 2);
 }
-export function minElArray(n){
-    return Math.min(...n)
+export function minElArray(n) {
+  return Math.min(...n);
 }
-export function maxElArray(n){
-    return Math.max(...n)
+export function maxElArray(n) {
+  return Math.max(...n);
 }
-export function diff(n1,n2){
-    return Math.abs(n1-n2)
+export function diff(n1, n2) {
+  return Math.abs(n1 - n2);
 }
-export function isWord(str){
-    return str.length > 1;
+export function isWord(str) {
+  return str.length > 1;
 }
-export function pow(a,x){
-    return Math.pow(a,x)
+export function pow(a, x) {
+  return Math.pow(a, x);
 }
-export function returnDateWeek(el){
-    const input = document.createElement('input');
-    input.type = 'date';
 
-    el.append(input);
-    const list = [];
-    const button = document.createElement('button');
-    button.innerHTML = 'to know';
-    el.append(button);
-    const listHTML = document.createElement('div');
-    el.append(listHTML);
-    button.addEventListener('click',()=>{
-        let date = new Date(input.value);
-        let options = { weekday: "long" };
-        const dayWeek = `${new Intl.DateTimeFormat("ru-US", options).format(date)}   
-        ${(input.value).split('-').reverse().join('-')}`
-        list.push(dayWeek);
-
-        input.value = '';
-        listHTML.innerHTML = `<ol>${list.map((el)=>`<li>${el}</li>`).join('')}</ol>`
-    })
+export function returnDateWeek(el) {
+  const input = document.createElement("input");
+  input.type = "date";
+  el.append(input);
+  const list = [];
+  const button = document.createElement("button");
+  button.innerHTML = "to know";
+  el.append(button);
+  const listHTML = document.createElement("div");
+  el.append(listHTML);
+  button.addEventListener("click", () => {
+    let date = new Date(input.value);
+    let options = { weekday: "long" };
+    const dayWeek = `${new Intl.DateTimeFormat("ru-US", options).format(
+      date,
+    )}   
+    
+        ${input.value.split("-").reverse().join("-")}`;
+    list.push(dayWeek);
+    input.value = "";
+    listHTML.innerHTML = `<ol>${list
+      .map((el) => `<li>${el}</li>`)
+      .join("")}</ol>`;
+  });
 }
--- src/homeworksOtus.test.js
+++ src/homeworksOtus.test.js
@@ -1,146 +1,170 @@
-import { sumNum,maxNum,returnMounth,sumFrom50_Before100,multiplicationTableForSeven,
-    averageAllOddNum,objectUser,makeInteractiveList,doubleArray,sumArray,minElArray,
-    maxElArray,diff,isWord,pow,returnDateWeek } from './homeworksOtus'
+import {
+  sumNum,
+  maxNum,
+  returnMounth,
+  sumFrom50_Before100,
+  multiplicationTableForSeven,
+  averageAllOddNum,
+  objectUser,
+  makeInteractiveList,
+  doubleArray,
+  sumArray,
+  minElArray,
+  maxElArray,
+  diff,
+  isWord,
+  pow,
+  returnDateWeek,
+} from "./homeworksOtus";
+describe("test code", () => {
+  it("return sum num", () => {
+    expect(sumNum("777")).toEqual(21);
+  });
+  it("return max num", () => {
+    expect(maxNum(12, 122)).toEqual(122);
+  });
+  it("return mounth", () => {
+    expect(returnMounth(5)).toEqual("май");
+  });
+  it("return sum from 50 before 100", () => {
+    expect(sumFrom50_Before100()).toEqual(3875);
+  });
+  it("return multiplication Table For Seven", () => {
+    expect(multiplicationTableForSeven()).toEqual([
+      "7 * 1 = 7",
+      "7 * 2 = 14",
+      "7 * 3 = 21",
+      "7 * 4 = 28",
+      "7 * 5 = 35",
+      "7 * 6 = 42",
+      "7 * 7 = 49",
+      "7 * 8 = 56",
+      "7 * 9 = 63",
+      "7 * 10 = 70",
+    ]);
+  });
+  it("return average All Odd Number", () => {
+    expect(averageAllOddNum(10)).toEqual(6);
+  });
+  it("return objectUser_Admin", () => {
+    //???
+    expect(objectUser("5")).toEqual({ role: "admin", age: "5" });
+  });
+});
+describe("homework number 7", () => {
+  let div;
+  let button;
+  let input;
+  beforeEach(() => {
+    div = document.createElement("div");
+    makeInteractiveList(div);
+    button = div.querySelector("button");
+    input = div.querySelector("input");
+  });
+  function getLi() {
+    return [...div.querySelectorAll("li")].map((li) => li.innerHTML);
+  }
+  it("is a function", () => {
+    expect(makeInteractiveList).toBeInstanceOf(Function);
+  });
+  it("make initial markup", () => {
+    expect(div.querySelector("input")).toBeTruthy();
+    expect(button).toBeTruthy();
+    expect(button.innerHTML).toBe("Add");
+    expect(button.hidden).toBe(true);
+    expect(div.querySelectorAll("li").length).toBe(3);
+    expect(getLi()).toEqual(["1", "22", "333"]);
+  });
+  it("shows or hides the button based on the input content", () => {
+    input.value = "123";
+    input.dispatchEvent(new Event("input"));
+    expect(button.hidden).toBe(false);
 
-describe('test code' , ()=>{
-    it('return sum num', ()=>{
-        expect(sumNum('777')).toEqual(21);
-    });
-    it('return max num', ()=>{
-        expect(maxNum(12,122)).toEqual(122);
-    });
-    it('return mounth',()=>{
-        expect(returnMounth(5)).toEqual('май')
-    })
-    it('return sum from 50 before 100',()=>{
-        expect(sumFrom50_Before100()).toEqual(3875)
-    })
-    it('return multiplication Table For Seven',()=>{
-        expect(multiplicationTableForSeven()).toEqual(['7 * 1 = 7', '7 * 2 = 14', '7 * 3 = 21', '7 * 4 = 28', '7 * 5 = 35', 
-        '7 * 6 = 42', '7 * 7 = 49', '7 * 8 = 56', '7 * 9 = 63', '7 * 10 = 70'])
-    })
-    it('return average All Odd Number',()=>{
-        expect(averageAllOddNum(10)).toEqual(6)
-    })
-    it('return objectUser_Admin',()=>{//???
-        expect(objectUser('5')).toEqual({role: 'admin', age: '5'})
-    })
-})
-describe('homework number 7', () =>{
-    let div;
-    let button;
-    let input;
-    beforeEach(()=>{
-        div = document.createElement('div');
-        makeInteractiveList(div);
-        button = div.querySelector('button');
-        input = div.querySelector("input");
-    })
-    function getLi(){
-        return [...div.querySelectorAll("li")].map((li) => li. innerHTML);
-    }
-    it('is a function', () => {
-        expect(makeInteractiveList).toBeInstanceOf(Function);
-    })
-    it('make initial markup', () => {
-        expect(div.querySelector('input')).toBeTruthy();
-        expect(button).toBeTruthy();
-        expect(button.innerHTML).toBe('Add');
-        expect(button.hidden).toBe(true);
-        expect(div.querySelectorAll('li').length).toBe(3);
-        expect(getLi()).toEqual(['1','22','333']);
-    });
-    it("shows or hides the button based on the input content",()=>{
-        input.value = "123";
-        input.dispatchEvent(new Event("input"));
-        expect(button.hidden).toBe(false);
+    div.querySelector("input").value = "";
+    input.dispatchEvent(new Event("input"));
+    expect(button.hidden).toBe(true);
+  });
+  it("adds new paragraph on button click", () => {
+    input.value = "123";
+    input.dispatchEvent(new Event("input"));
+    button.click();
+    expect(getLi()).toEqual(["1", "22", "333", "123"]);
+    expect(input.value).toBe("");
+    expect(button.hidden).toBe(true);
+  });
+  it("removes first paragraph if total p number is more than 5", () => {
+    const text1 = "4444";
+    input.value = text1;
+    input.dispatchEvent(new Event("input"));
+    button.click();
 
-        div.querySelector("input").value = "";
-        input.dispatchEvent(new Event("input"));
-        expect(button.hidden).toBe(true);
-    })
-    it("adds new paragraph on button click",()=>{
-        input.value = "123";
-        input.dispatchEvent(new Event("input"));
-        button.click();
-        expect(getLi()).toEqual(['1','22','333','123']);
-        expect(input.value).toBe('');
-        expect(button.hidden).toBe(true);
-    })
-    it("removes first paragraph if total p number is more than 5",()=>{
-        const text1 = "4444";
-        input.value = text1;
-        input.dispatchEvent(new Event("input"));
-        button.click();
+    const text2 = "55555";
+    input.value = text2;
+    input.dispatchEvent(new Event("input"));
+    button.click();
 
-        const text2 = "55555";
-        input.value = text2;
-        input.dispatchEvent(new Event("input"));
-        button.click();
+    const text3 = "666666";
+    input.value = text3;
+    input.dispatchEvent(new Event("input"));
+    button.click();
 
-        const text3 = "666666";
-        input.value = text3;
-        input.dispatchEvent(new Event("input"));
-        button.click();
-
-        expect(getLi()).toEqual(['22','333',text1,text2,text3]);
-
-    })
-})
-describe('homework number 5',()=>{
-    let array = [5,2,3,7,3,10,3,10,3,4];
-    let double = array.map((el)=>el*2);
-    it('summa Array',()=>{
-        expect(sumArray(array)).toBe(50)
-    });
-    it('double Array',()=>{
-        expect(doubleArray(array)).toStrictEqual(double)
-    });
-    it('min Array',()=>{
-        expect(minElArray(array)).toBe(2);
-    });
-    it('max Array',()=>{
-        expect(maxElArray(array)).toBe(10);
-    });
-})
-describe('homework number 6',()=>{
-    it('test for diff',()=>{
-        expect(diff(10,6)).toBe(4);
-        expect(diff(6,10)).toBe(4);
-    });
-    it('is Word?',()=>{
-        expect(isWord('world')).toBe(true);
-        expect(isWord('d')).toBe(false);
-    });
-    it('pow',()=>{
-        expect(pow(2,5)).toBe(32);
-        expect(pow(8,2)).toBe(64);
-    });
-})
-describe('homework number 8',()=>{
-    let div;
-    let button;
-    let input;
-    beforeEach(()=>{
-        div = document.createElement('div');
-        returnDateWeek(div);
-        button = div.querySelector('button');
-        input = div.querySelector('input');
-    });
-    function getLi(){
-        return [...div.querySelectorAll("li")].map((li) => li.innerHTML);
-    }
-    it('make initial markup',()=>{
-        expect(input).toBeTruthy();
-        expect(button).toBeTruthy();
-        expect(button.innerHTML).toBe('to know');
-    })
-    it("adds new days on button click",()=>{
-        input.value = '2023-11-17';
-        input.dispatchEvent(new Event("input"));
-        button.click();
-        expect(getLi().length).toBe(1);
-        expect(input.value).toBe('');
-        expect(getLi()).toEqual(['пятница   \n        17-11-2023'])
-    })
-})
+    expect(getLi()).toEqual(["22", "333", text1, text2, text3]);
+  });
+});
+describe("homework number 5", () => {
+  let array = [5, 2, 3, 7, 3, 10, 3, 10, 3, 4];
+  let double = array.map((el) => el * 2);
+  it("summa Array", () => {
+    expect(sumArray(array)).toBe(50);
+  });
+  it("double Array", () => {
+    expect(doubleArray(array)).toStrictEqual(double);
+  });
+  it("min Array", () => {
+    expect(minElArray(array)).toBe(2);
+  });
+  it("max Array", () => {
+    expect(maxElArray(array)).toBe(10);
+  });
+});
+describe("homework number 6", () => {
+  it("test for diff", () => {
+    expect(diff(10, 6)).toBe(4);
+    expect(diff(6, 10)).toBe(4);
+  });
+  it("is Word?", () => {
+    expect(isWord("world")).toBe(true);
+    expect(isWord("d")).toBe(false);
+  });
+  it("pow", () => {
+    expect(pow(2, 5)).toBe(32);
+    expect(pow(8, 2)).toBe(64);
+  });
+});
+describe("homework number 8", () => {
+  let div;
+  let button;
+  let input;
+  beforeEach(() => {
+    div = document.createElement("div");
+    returnDateWeek(div);
+    button = div.querySelector("button");
+    input = div.querySelector("input");
+  });
+  function getLi() {
+    return [...div.querySelectorAll("li")].map((li) => li.innerHTML);
+  }
+  it("make initial markup", () => {
+    expect(input).toBeTruthy();
+    expect(button).toBeTruthy();
+    expect(button.innerHTML).toBe("to know");
+  });
+  it("adds new days on button click", () => {
+    input.value = "2023-11-17";
+    input.dispatchEvent(new Event("input"));
+    button.click();
+    expect(getLi().length).toBe(1);
+    expect(input.value).toBe("");
+    expect(getLi()).toEqual(["пятница   \n        17-11-2023"]);
+  });
+});
